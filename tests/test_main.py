07b6551a520e222d459a70f724e77db7b34646d4
Kesley Rana <kesley_rana@sfu.ca>
2025-10-27 13:52:22 -0700

    fix bugs int the main and test.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,7 +8,7 @@ def test_overpayment():
     assert run_vending_machine([25, 25, 10]) == 10
 
 def test_multiple_small_coins():
-    assert run_vending_machine([10, 10, 5, 5, 5]) == 0
+    assert run_vending_machine([10, 10, 5, 5, 5]) == 15
 
 def test_invalid_coin():
     assert run_vending_machine([3, 1, 7, 25, 25]) == 0
